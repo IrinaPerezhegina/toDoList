acb91b6a6a2d916f4f715c8acc94ad32b314b529
IrinaPerezhegina <perezeginaiv@mail.ru>
2024-01-12 13:48:35 +0500

    add cors

--- server/app.js
+++ server/app.js
@@ -5,13 +5,13 @@ const chalk = require("chalk");
 const path = require("path");
 const routes = require("./routes");
 require("dotenv").config({ path: path.resolve(__dirname, "./.env.example") });
-
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// app.use(cors());
+app.use(cors());
 app.use("/api", routes);
 console.log(routes);
 mongoose.set("strictQuery", true);
